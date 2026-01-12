0805e4a2d4cc16449869cf580fdc5f6ba3d26f3a
Bryce Large <bryce@jiro.co.za>
2026-01-12 21:35:14 +0200

    change port

--- scripts/generate_haproxy_config.sh
+++ scripts/generate_haproxy_config.sh
@@ -81,7 +81,8 @@ echo "[haproxy] Generating configuration..." | ts '%Y-%m-%d %H:%M:%S'
 
 # Set the correct port for HTTP/3 alt-svc header based on MIXED_SSL_MODE
 if [ "$MIXED_SSL_MODE" = "true" ]; then
-    QUIC_PORT="8443"
+    #QUIC_PORT="8443"
+    QUIC_PORT="443"
 else
     QUIC_PORT="443"
 fi
